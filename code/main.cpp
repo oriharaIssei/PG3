be604944e4e521664d2745c335b4f865359c6c3f
Amaimono <k023g0048@g.neec.ac.jp>
2024-09-30 13:47:41 +0900

    01_02

--- code/main.cpp
+++ code/main.cpp
@@ -1,36 +1,60 @@
 #include <cstdio>
+#include <random>
 
-#include <algorithm>
+#include <ctime>
+#include <string>
+#include <stdio.h>
+#include <stdint.h>
+#include <iostream>
 
-const float firstRecursiveSalary = 100.0f;
-const float salary = 1072.0f;
+// 乱数生成器を初期化
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_int_distribution<> randDice(1,6);
 
-float RecursiveSalary(int workHours,float salary){
-	workHours -= 1;
-	if(workHours <= 0){
-		return 0;
-	}
-	return salary + (salary + RecursiveSalary(workHours,(salary * 2.0f) - 50.0f));
+int32_t input;
+
+int32_t DiceRoll(){
+	return randDice(gen);
 }
-float NormalSalary(int workHours,float salary){
-	return salary * static_cast<float>(workHours);
+
+void TimedCall(void (*func)(),int32_t sec){
+	time_t current = time(nullptr);
+	time_t end = current + sec;
+
+	while(current <= end){
+		current = time(nullptr);
+	}
+
+	func();
+}
+
+void Answer(){
+	bool isEvenNum = DiceRoll() % 2;
+
+	if(isEvenNum == input){
+		std::cout << "正解 !!!" << std::endl;
+		return;
+	}
+	std::cout << "不正解" << std::endl;
+};
+
+void GameLoop(){
+	std::cout << "奇数か偶数か． 奇数だと思うならなら 0 ,偶数だと思うなら 1 を入力してください\n" << std::endl;
+	std::cin >> input;
+	if(input != 0 && input != 1){
+		std::cout << " その回答は 存在しません " << std::endl;
+		return;
+	}
+
+	std::cout << " 正解は ...  \n";
+
+	TimedCall(Answer,3);
 }
 
 int main(){
-	int workTime = 1;
-	float recursiveSalary = RecursiveSalary(workTime,firstRecursiveSalary);
-	float normalSalary = NormalSalary(workTime,salary);
-
-	while(recursiveSalary < normalSalary){
-		++workTime;
-		recursiveSalary = RecursiveSalary(workTime,firstRecursiveSalary);
-		normalSalary = NormalSalary(workTime,salary);
-		printf("%d Hours \n",workTime);
-		printf("normalSalary = %f\n",normalSalary);
-		printf("recursiveSalary = %f\n",recursiveSalary);
-		printf("--------------------------------------------------\n");
+	while(true){
+		GameLoop();
 	}
-
-	printf("%d Hours, %f Salary",workTime,recursiveSalary);
 	return 0;
 }
