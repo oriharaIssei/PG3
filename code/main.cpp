3591eaf31471d3820d95c9c62a56b37b3b375471
Amaimono <k023g0048@g.neec.ac.jp>
2024-10-07 10:16:02 +0900

    01_04

--- code/main.cpp
+++ code/main.cpp
@@ -2,59 +2,68 @@
 #include <random>
 
 #include <ctime>
-#include <string>
-#include <stdio.h>
-#include <stdint.h>
+#include <functional>
 #include <iostream>
+#include <stdint.h>
+#include <stdio.h>
+#include <string>
 
 // 乱数生成器を初期化
 std::random_device rd;
 std::mt19937 gen(rd());
 std::uniform_int_distribution<> randDice(1,6);
 
-int32_t input;
 
-int32_t DiceRoll(){
+
+int32_t DiceRoll()
+{
 	return randDice(gen);
 }
 
-void TimedCall(void (*func)(),int32_t sec){
+void TimedCall(std::function<void()> func,int32_t sec)
+{
 	time_t current = time(nullptr);
 	time_t end = current + sec;
 
-	while(current <= end){
+	while(current <= end)
+	{
 		current = time(nullptr);
 	}
 
 	func();
 }
 
-void Answer(){
-	bool isEvenNum = DiceRoll() % 2;
-
-	if(isEvenNum == input){
+void Answer(int32_t input,int32_t randNum)
+{
+	if(randNum == input)
+	{
 		std::cout << "正解 !!!" << std::endl;
 		return;
 	}
 	std::cout << "不正解" << std::endl;
 };
 
-void GameLoop(){
+void MainLoop()
+{
+	int32_t input,randNum;
 	std::cout << "奇数か偶数か． 奇数だと思うならなら 0 ,偶数だと思うなら 1 を入力してください\n" << std::endl;
 	std::cin >> input;
-	if(input != 0 && input != 1){
+	if(input != 0 && input != 1)
+	{
 		std::cout << " その回答は 存在しません " << std::endl;
 		return;
 	}
 
 	std::cout << " 正解は ...  \n";
-
-	TimedCall(Answer,3);
+	randNum = DiceRoll() % 2;
+	TimedCall([input,randNum]() { Answer(input,randNum); },3);
 }
 
-int main(){
-	while(true){
-		GameLoop();
+int main()
+{
+	while(true)
+	{
+		MainLoop();
 	}
 	return 0;
 }
