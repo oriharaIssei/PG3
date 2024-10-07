51edbbe2439b73f25a284713bb7c5d2398ba7bf4
Amaimono <k023g0048@g.neec.ac.jp>
2024-10-07 10:07:10 +0900

    01_02

--- code/main.cpp
+++ code/main.cpp
@@ -1,60 +1,28 @@
 #include <cstdio>
-#include <random>
 
-#include <ctime>
-#include <string>
-#include <stdio.h>
-#include <stdint.h>
-#include <iostream>
+#include <algorithm>
 
-// 乱数生成器を初期化
-std::random_device rd;
-std::mt19937 gen(rd());
-std::uniform_int_distribution<> randDice(1,6);
-
-int32_t input;
-
-int32_t DiceRoll(){
-	return randDice(gen);
-}
-
-void TimedCall(void (*func)(),int32_t sec){
-	time_t current = time(nullptr);
-	time_t end = current + sec;
-
-	while(current <= end){
-		current = time(nullptr);
-	}
-
-	func();
-}
-
-void Answer(){
-	bool isEvenNum = DiceRoll() % 2;
-
-	if(isEvenNum == input){
-		std::cout << "正解 !!!" << std::endl;
-		return;
-	}
-	std::cout << "不正解" << std::endl;
+template <typename T>
+T Min(T a,T b)
+{
+	return std::min(a,b);
 };
-
-void GameLoop(){
-	std::cout << "奇数か偶数か． 奇数だと思うならなら 0 ,偶数だと思うなら 1 を入力してください\n" << std::endl;
-	std::cin >> input;
-	if(input != 0 && input != 1){
-		std::cout << " その回答は 存在しません " << std::endl;
-		return;
-	}
-
-	std::cout << " 正解は ...  \n";
-
-	TimedCall(Answer,3);
+template <>
+char Min(char a,char b)
+{
+	printf_s("Not of Number.\n");
+	return 'n';
 }
 
-int main(){
-	while(true){
-		GameLoop();
-	}
+int main()
+{
+	printf_s("Min<int> (36,48) = %d\n",Min<int>(36,48));
+
+	printf_s("Min<float> (0.1f,12.0f) = %f\n",Min<float>(0.1f,12.0f));
+
+	printf_s("Min<double> (0.34,3.14) = %f\n",Min<double>(34,3.14));
+
+	printf_s("Min<char> (x,y) = %c\n",Min<char>('x','y'));
+
 	return 0;
 }
