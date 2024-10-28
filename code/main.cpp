f21f802871f555353e9de00ee9bb92a9386eb5a3
Amaimono <k023g0048@g.neec.ac.jp>
2024-10-28 11:42:01 +0900

    02_02

--- code/main.cpp
+++ code/main.cpp
@@ -1,5 +1,29 @@
 #include <cstdio>
+#include <algorithm>
+
+#include <stdint.h>
+
+template <typename T>
+T Min(T a,T b){
+	return (std::min)(a,b);
+}
 
 int main(){
+	int32_t intA = 1;
+	int32_t intB = 56;
 
+	float fA = 0.1212f;
+	float fB = -124.0f;
+
+	double dA = 0.221;
+	double dB = 1241211.0;
+
+	printf("int\n  A = %d, B = %d, Min = %d\n",
+		   intA,intB,Min(intA,intB));
+
+	printf("float\n  A = %f, B = %f, Min = %f\n",
+		   fA,fB,Min(fA,fB));
+
+	printf("double\n  A = %lf, B = %lf, Min = %lf\n",
+		   dA,dB,Min(dA,dB));
 }
