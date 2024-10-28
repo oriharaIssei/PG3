9aab1d0ac7d08bba5f7551932d6b2a02f0f4cf64
Amaimono <k023g0048@g.neec.ac.jp>
2024-10-28 12:02:34 +0900

    02_03

--- code/main.cpp
+++ code/main.cpp
@@ -1,5 +1,20 @@
 #include <cstdio>
+#include <memory>
+
+#include "Figtures.h"
 
 int main(){
+	std::unique_ptr<IFigure> figure;
+	figure = std::make_unique<Triangle>();
+	figure->Update();
+
+	figure = std::make_unique<Square>();
+	figure->Update();
+	
+	figure = std::make_unique<Pentagon>();
+	figure->Update();
+
+	figure = std::make_unique<Circle>();
+	figure->Update();
 
 }
