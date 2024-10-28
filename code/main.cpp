59277e6573ab344613e1fc388caa90d345c4e09e
Amaimono <k023g0048@g.neec.ac.jp>
2024-10-28 12:09:40 +0900

    fix : conbination

--- code/main.cpp
+++ code/main.cpp
@@ -3,9 +3,9 @@
 
 #include <stdint.h>
 
-template <typename T>
-T Min(T a,T b){
-	return (std::min)(a,b);
+template <typename Return,typename aT,typename bT>
+Return Min(aT a,bT b){
+	return static_cast<Return>(std::min(static_cast<Return>(a),static_cast<Return>(b)));
 }
 
 int main(){
@@ -19,11 +19,20 @@ int main(){
 	double dB = 1241211.0;
 
 	printf("int\n  A = %d, B = %d, Min = %d\n",
-		   intA,intB,Min(intA,intB));
+		   intA,intB,Min<int32_t>(intA,intB));
 
 	printf("float\n  A = %f, B = %f, Min = %f\n",
-		   fA,fB,Min(fA,fB));
+		   fA,fB,Min<float>(fA,fB));
 
 	printf("double\n  A = %lf, B = %lf, Min = %lf\n",
-		   dA,dB,Min(dA,dB));
+		   dA,dB,Min<double>(dA,dB));
+
+	printf("int : float\n  int = %d, float = %f, Min = %f\n",
+		   intA,fB,Min<float>(intA,fB));
+
+	printf("float : double\n  float = %f, double = %lf, Min = %lf\n",
+		   fA,dB,Min<double>(fA,dB));
+
+	printf("int : double\n  A = %d, B = %lf, Min = %lf\n",
+		   intA,dB,Min<double>(intA,dB));
 }
