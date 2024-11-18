499950515f93461b42a76c1741834a4434781144
Amaimono <k023g0048@g.neec.ac.jp>
2024-11-18 12:54:08 +0900

    Load Text

--- code/main.cpp
+++ code/main.cpp
@@ -1,5 +1,52 @@
 #include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+const std::string fileName = "PG3_2024_03_02.txt";
+std::vector<std::string> LoadText(const std::string& _fileName = fileName);
 
 int main(){
+	std::vector<std::string> numbers = LoadText(fileName);
 
+	return 0;
+}
+
+std::vector<std::string> LoadText(const std::string& _fileName){
+	std::ifstream file(_fileName);
+
+	std::vector<std::string> result;
+
+	if(!file.is_open()){
+		std::cerr << "ファイルを開けませんでした。" << std::endl;
+		return result;
+	}
+
+	std::string line;
+	std::string token;
+	bool insideQuotes = false;
+	while(std::getline(file,line)){
+		for(char c : line){
+			if(c == '"'){
+				if(insideQuotes){
+					// 追加
+					result.push_back(token);
+					// 初期化
+					token = "";
+					insideQuotes = false;
+				}
+				insideQuotes = true;
+			} else if(c == ','){
+				insideQuotes = false;
+			} else if(insideQuotes){
+				// "の中の文字をコピー
+				token += c;
+			}
+		}
+	}
+
+	file.close();
+
+	return result;
 }
