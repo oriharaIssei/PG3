4a50caf0d7e8f52c04f71b9cf1904d0fed413688
OriharaIssei <k023g0048@g.neec.ac.jp>
2024-12-19 14:05:20 +0900

    project 設定

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+workspace "PG3"
+    configurations { "Debug", "Release" }
+    platforms {"x64" }
+
+    location "project"
+    basedir "project"
+
+project "PG3"
+    kind "ConsoleApp"
+    language "C++"
+    
+    -- 出力先 と 中間 フォルダの指定
+    targetdir "generated/outputs/%{cfg.buildcfg}"
+    objdir "generated/obj/%{cfg.buildcfg}"
+    targetname "PG3"  -- Exe名をworkspace名に設定
+
+    cppdialect "C++20"
+    characterset "Unicode"
+
+    files { "**.h", "**.cpp" }
+
+    buildoptions { "/utf-8" }
+
+    filter "platforms:x64"
+        architecture "x64"
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
