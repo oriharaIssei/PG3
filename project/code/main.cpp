f524353f051fa564fca91a8f9d930b163f3fa4f6
OriharaIssei <k023g0048@g.neec.ac.jp>
2025-01-27 10:04:00 +0900

    07_01

--- project/code/main.cpp
+++ project/code/main.cpp
@@ -1,6 +1,36 @@
+#include <chrono>
 #include <cstdio>
+#include <string>
 
+// コピーとムーブの速度比較
 int main() {
+    // ベースとなる文字列
+    const int stringCount = 100000;
+    std::string baseStr(100000, 'a');
+    // コピーした文字列
+    std::string copyStr;
+    // ムーブした文字列
+    std::string moveStr;
+
+    // コピー
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        copyStr    = baseStr;
+        auto end   = std::chrono::high_resolution_clock::now();
+        auto msec  = static_cast<float>(std::chrono::duration<float>(end - start).count());
+
+        printf("copy: %f msec\n", msec);
+    }
+
+    // ムーブ
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        moveStr    = std::move(baseStr);
+        auto end   = std::chrono::high_resolution_clock::now();
+        auto msec  = static_cast<float>(std::chrono::duration<float>(end - start).count());
+
+        printf("move: %f msec\n", msec);
+    }
 
     return 0;
 }
